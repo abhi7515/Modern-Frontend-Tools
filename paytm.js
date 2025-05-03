4831107909ff40ef846a1ff1527c39a71de127b5
Abhinav Shukla <abhishukla965@gmail.com>
2025-05-03 15:10:35 +0530

    Create paytm.js

---
+++ paytm.js
@@ -0,0 +1,14 @@
+Paytm Interview Experience
+
+1 Use Case for useContext + useReducer vs Redux, focus on middleware how they incecept the action after dispatch before reaching reducer and run side effects.
+2 Given a string leetcode  traverse it feom both right and left and swap vowels leotcede .
+3 how does react update multiple states efficiently, Batching of state updates in Render and custumization allowed by React 18 useTransition hook to delay paint.
+4 How to display 0th element from 50th element in a carousel, with scroll animation.
+5 multiple states have to ve updated in a search input tern and a flitered list so due to two state updation their is a jerky behavior usetransition to fix this jerk.
+6 What are 4 pillars of OOps and how are they implemented in js, focus on prototypes, show a example using __proto__ .
+7 if setTransition was not available use web workers
+8 How does event loop work and 2 questions on it using promises and settimeout
+9 working of useState  useState(() => "value") vs useState("value")
+10 Component lifecycle in React Functional Component, what is cleanup function when is it called, what are impacts in it if dependency array is modified.
+11 Event Capturing vs bubbling
+12  What are generater functions
