510dd375e2eba1c67cd44452964b836b0557d6e7
Abhinav Shukla <abhishukla965@gmail.com>
2025-06-21 17:43:36 +0530

    Update ops.js

--- ops.js
+++ ops.js
@@ -31,3 +31,40 @@ function sleep(ms) {
   sleep(1000).then(() => console.log(i));
   }
 })();
+
+----------using map-------------------
+
+arr.forEach((item, index) => {
+  setTimeout(() => {
+    console.log(item});
+  }, index * 1000);
+});
+
+------------using bind method------------
+
+// .bind(null, i) returns a new function where index is set to the current value of i
+// So in each iteration, the timer gets a different copy of the function with its own fixed index
+
+for (var i = 0; i < arr.length; i++) {
+  setTimeout(
+    function(index) {
+      console.log(`bind direct: ${arr[index]}`);
+    }.bind(null, i),
+    i * 1000
+  );
+}
+
+for (var i = 0; i < arr.length; i++) {
+  setTimeout(
+    (function(index) {
+      return function () {
+        console.log(index);
+      };
+    })(i),
+    i * 1000
+  );
+}
+
+
+
+
