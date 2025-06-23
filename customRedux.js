55ff2ed701493da0e6c54c64b2e1aa4d3fbeb13d
Abhinav Shukla <abhishukla965@gmail.com>
2025-06-24 03:26:18 +0530

    Create customRedux.js

---
+++ customRedux.js
@@ -0,0 +1,9 @@
+ Second Round:
+🌐 Concentrated on frontend development.
+🔍 Asked questions about React, components, optimization, and how the web works.
+🔄 Explored the differences between a React app and a normal app.
+🔧 Lots of questions about Redux, including its architecture, sagas, and thunks.
+💡 Finally, I was given a question to implement a custom Redux store. Managed to solve it with some help from the interviewer 😂. He was very helpful and patient. 🙏
+
+prepare me for this
+
